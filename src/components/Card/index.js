08ce5bab6cc65ec614ddc4603d81a62b427245c5
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2019-08-18 14:09:32 -0700

    #3 loaded cards with dummy data for now

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,16 +1,21 @@
 import React from "react";
+import Truncate from 'react-truncate';
+import "./style.css";
 
-function Card({ icon, title, children }) {
+function Card({ children, price, image }) {
   return (
-    <div className="card mt-4">
-      <div className="card-header">
-        <h3>
-          <strong>
-            <i className={`fa fa-${icon}`} aria-hidden="true" /> {title}
-          </strong>
-        </h3>
+    <div className="col-lg-3 col-md-4 col-sm-6 mb-5 mx-auto">
+      <div className="text-center">
+        <img className="img-fluid card-img" src={image} alt={children} />
       </div>
-      <div className="card-body">{children}</div>
+        <h6 className="card-title">
+          <strong>
+            <Truncate lines={2}>
+              {children}
+            </Truncate>
+          </strong>
+        </h6>
+        <span>${price}</span>
     </div>
   );
 }
