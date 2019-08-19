b008492aa3b1fb5d503ecac81fd5d1d133e79d20
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2019-08-19 08:05:45 -0700

    #3 switched out for global css style

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -33,18 +33,26 @@ class Nav extends Component {
   render() {
     return (
       <div>
-        <div className="row fixed-top" style={{ "backgroundColor": "#F8F9FA"}}>
+        <div className="row fixed-top theme-background-color">
           <div className="offset-2 col-8">
             <nav className="navbar navbar-expand-lg navbar-light bg-light w-100 d-flex justify-content-between">
               <Link className="navbar-brand" to="/">
-                <img className="navbar-logo" src={process.env.PUBLIC_URL + "fantasticHeadBandsLogo.png"} alt="Logo" />
+                <img
+                  className="navbar-logo"
+                  src={process.env.PUBLIC_URL + "fantasticHeadBandsLogo.png"}
+                  alt="Logo"
+                />
               </Link>
               <div className="" id="navbarNav">
                 <ul className="navbar-nav">
                   <li className="nav-item">
                     <Link
                       onClick={this.toggleNav}
-                      className={window.location.pathname === "/saved" ? "nav-link active" : "nav-link"}
+                      className={
+                        window.location.pathname === "/cart"
+                          ? "nav-link active"
+                          : "nav-link"
+                      }
                       to="/cart"
                     >
                       Cart
@@ -55,8 +63,7 @@ class Nav extends Component {
             </nav>
           </div>
         </div>
-        <div style={{ "height": "86px"}}>
-        </div>
+        <div style={{ height: "86px" }} />
       </div>
     );
   }
