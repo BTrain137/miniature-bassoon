d9e20aa9f18a891af5133d35f6948625192e5436
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2019-08-21 23:15:43 -0700

    CROS error but should style work once course is completed

--- src/components/ForSale/index.js
+++ src/components/ForSale/index.js
@@ -2,26 +2,33 @@ import React, { Component } from "react";
 import Card from "../Card";
 import "./style.css";
 
-import data from "./dummyData.json";
+// import data from "./dummyData.json";
+
+const all_product_url =
+  "https://7io32bkt5j.execute-api.us-west-2.amazonaws.com/dev/shopify/all-products";
 
 class ForSale extends Component {
   state = {
-    cards: [],
+    cards: []
   };
 
   componentDidMount() {
-    const productCards = data.products.map(function(datam) {
-      return {
-        title: datam.title,
-        handle: datam.handle,
-        image: datam.image.src,
-        price: datam.variants[0].price,
-      };
+    fetch(all_product_url, items => {
+
+      const productCards = items.map(function(datam) {
+        return {
+          title: datam.title,
+          handle: datam.handle,
+          image: datam.image.src,
+          price: datam.variants[0].price
+        };
+      });
+    // console.log(productCards);
+      this.setState({
+        cards: productCards
+      });
     });
 
-    this.setState({
-      cards: productCards,
-    });
   }
 
   render() {
@@ -29,18 +36,20 @@ class ForSale extends Component {
       <section className="for-sale mt-4">
         <div className="text-center mb-3">
           <h3 className="title">TRENDING</h3>
-          <div className="block-note">{ this.props.children }</div>
+          <div className="block-note">{this.props.children}</div>
         </div>
         <div className="container mx-auto row">
-          {this.state.cards.map(function(card, i){
-            return  <Card 
-                      key={i}
-                      price={card.price}
-                      image={card.image}
-                      handle={card.handle}
-                    >
-                      { card.title }
-                    </Card>
+          {this.state.cards.map(function(card, i) {
+            return (
+              <Card
+                key={i}
+                price={card.price}
+                image={card.image}
+                handle={card.handle}
+              >
+                {card.title}
+              </Card>
+            );
           })}
         </div>
       </section>
