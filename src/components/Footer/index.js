c18a026f476cfd6905c4ce8a850428b840f0fa52
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2019-08-21 23:16:09 -0700

    Minor CSS and HTML changes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,8 +7,8 @@ function Footer() {
       <hr />
       <div className="container">
         <div className="row mt-4">
-          <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-            <div class="text-center">
+          <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+            <div className="text-center">
               <img
                 className="navbar-logo"
                 src={process.env.PUBLIC_URL + "fantasticHeadBandsLogo.png"}
@@ -16,43 +16,42 @@ function Footer() {
               />
             </div>
           </div>
-          <div className="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-            <ul class="list-icon">
+          <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+            <ul className="list-icon">
               <li>
-                <span class="icon pe-7s-map-marker">&nbsp;</span>
+                <span className="icon pe-7s-map-marker">&nbsp;</span>
                 184 Main Rd E, St Albans <br />
                 VIC 3021, Australia
               </li>
             </ul>
           </div>
-          <div className="col-lg-3 col-md-2 col-sm-6 col-xs-12">
-            <ul class="list-icon">
+          <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+            <ul className="list-icon">
               <li>
-                <span class="icon pe-7s-mail">&nbsp;</span>
+                <span className="icon pe-7s-mail">&nbsp;</span>
                 <a href="mailto:contact@company.com">contact@company.com</a>
               </li>
               <li>
-                <span class="icon pe-7s-call">&nbsp;</span>{" "}
+                <span className="icon pe-7s-call">&nbsp;</span>{" "}
                 <a href="tel:+0012233456">+001 2233 456</a>
               </li>
             </ul>
           </div>
-          <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+          <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12">
             <p>
               Subscribe to our newsletter and get 10% off your first purchase
             </p>
             <div>
-              <div class="block newsletter">
-                <div class="content">
+              <div className="block newsletter">
+                <div className="content">
                   <form
-                    class="form subscribe"
-                    novalidate="novalidate"
+                    className="form subscribe"
                     action="http://themes.magesolution.com/claue/m2/newsletter/subscriber/new/"
                     method="post"
                     id="newsletter-validate-detail-footer"
                   >
-                    <div class="field form-group newsletter">
-                      <div class="control">
+                    <div className="field form-group newsletter">
+                      <div className="control">
                         <input
                           name="email"
                           type="email"
@@ -61,21 +60,12 @@ function Footer() {
                         />
                       </div>
                     </div>
-                    <div class="actions">
-                      <button
-                        class="action subscribe btn btn-primary btn-sm"
-                        title="Subscribe"
-                        type="submit"
-                      >
-                        <span>Subscribe</span>
-                      </button>
-                    </div>
                   </form>
                 </div>
               </div>
             </div>
-            <p class="margin-top25">
-              <span class="footer-payment-img">&nbsp;</span>
+            <p className="mt-5">
+              <span className="footer-payment-img" style={{ "backgroundImage": "url("+ process.env.PUBLIC_URL + "./assets/images/sprites_flag.jpg)"}}>&nbsp;</span>
             </p>
           </div>
         </div>
